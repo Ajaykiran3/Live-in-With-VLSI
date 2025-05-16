6f977e83e4b6fa4455c404f3c5f040ce049d5711
Ajaykiran3 <23A95A0405@aec.edu.in>
2025-05-16 13:06:46 +0530

    Live in With VLSI

---
+++ Perl/Day 38 Comments/Commenting.pl
@@ -0,0 +1,12 @@
+#Comments:
+# 1st way of Commenting - Single Line Comments
+=Begin 2nd Way of Commenting - 
+                                Multi
+                                    Line
+                                        Comments
+=cut
+my $comment = <<"VLSI05";
+This is 3 rd way of Commenting 
+Multi Line Commenting
+VLSI05
+print $comment,"\n";
