bdc6da3a408a837e11a400dc53e1a520435ee4a8
Ajaykiran3 <23A95A0405@aec.edu.in>
2025-05-18 18:52:08 +0530

    Live in With VLSI

---
+++ Perl/Day 40 Datatypes/datatypes.pl
@@ -0,0 +1,11 @@
+# Data Types :
+# 1.Scalar ($):
+$scalar1 = "VLSI";
+$scalar2 = 4;
+print "$scalar1\n$scalar2\n";
+# 2.Arrays (@):
+@arr = ("V",'L',"S",'I',0,4);
+print "@arr\n";
+# 3.Hashes (%):
+%hash1 = ("VLSI" => 04,Python => 06 );
+print %hash1,"\n";
