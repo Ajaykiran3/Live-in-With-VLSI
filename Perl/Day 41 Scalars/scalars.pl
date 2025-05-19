a59443bed958a651eb7f7a689fb3211acf2f9824
Ajaykiran3 <23A95A0405@aec.edu.in>
2025-05-19 06:30:35 +0530

    Live in With VLSI

---
+++ Perl/Day 41 Scalars/scalars.pl
@@ -0,0 +1,21 @@
+# Scalars :
+# 1. Numerical Scalar :
+$num = 5;
+print "$num\n";
+#2. String Scalar :
+$str = "VLSI";
+print "$str\n";
+#3. vString :
+$vstr = v85.78.73.88;
+print "$vstr\n";
+#4 .Some Operations on Scalars :
+#4.1 Arithmetic operation(+,- etc):
+$num2 = 6;
+print $num+$num2,"\n";
+print $num-$num2,"\n";
+#4.2 Concatenation operation (. or ,):
+$concat = $num."-".$str."-".$vstr;
+print "$concat\n";
+#4.3 Repetation Operator :
+print "$num\n" x 4;
+
